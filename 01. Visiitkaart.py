f0c7fe787925760b17716148468db13b861ff700
Indrek Noortoots <nindrek@yahoo.ie>
2013-11-06 11:28:53 +0200

    uuendus

--- 01. Visiitkaart.py
+++ 01. Visiitkaart.py
@@ -5,6 +5,7 @@ email = raw_input("Sisesta oma e-mail: ")
 tel = raw_input("Sisesta oma telefoni nr: ")
 tekst = "Business card"
 tyhi = " "
+joon = "="
 print "|", tekst.center(60, "="), '|' #tyhi rida
 print "|", tyhi.center(60, " "), '|'
 print "|", nimi.center(60), "|"
@@ -12,4 +13,4 @@ print "|", ametinimi.center(60), "|"
 print "|", email.center(60), "|"
 print "|", tel.center(60), "|"
 print "|", tyhi.center(60, " "), '|'#tyhi rida
-print "|", tyhi.center(60, "="), '| \n'
+print "|", joon.center(60, "="), '| \n'
