6877d87d4a9a84feb0f151176d78c663666f123e
Indrek <nindrek@yahoo.ie>
2013-11-06 11:23:54 +0200

    Kodutöö 2.

---
+++ 01. Visiitkaart.py
@@ -0,0 +1,15 @@
+nimi = raw_input("Sisesta t2isnimi ")
+name = nimi.capitalize()
+ametinimi = raw_input("Sisesta oma ametinimi: ")
+email = raw_input("Sisesta oma e-mail: ")
+tel = raw_input("Sisesta oma telefoni nr: ")
+tekst = "Business card"
+tyhi = " "
+print "|", tekst.center(60, "="), '|' #tyhi rida
+print "|", tyhi.center(60, " "), '|'
+print "|", nimi.center(60), "|"
+print "|", ametinimi.center(60), "|"
+print "|", email.center(60), "|"
+print "|", tel.center(60), "|"
+print "|", tyhi.center(60, " "), '|'#tyhi rida
+print "|", tyhi.center(60, "="), '| \n'
